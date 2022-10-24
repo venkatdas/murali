2e35376c9f53199b2123e1375c46148f198244b9
venkat dasu <venkat3990@gmail.com>
2022-10-24 08:23:13 +0530

    murali new project

---
+++ startttonlabs.js
@@ -0,0 +1,21 @@
+const progress = document.querySelectorAll(".bar");
+
+var a= parseInt(document.getElementById("invalue").value);
+
+var b =parseInt(document.getElementById("maxvalue").value);
+function myFun(){
+    if(a>b){
+
+        alert("you enterned high number");
+        document.getElementById('a').value = '';
+        document.getElementById('b').focus();
+     }else{
+         alert("false")
+     }
+
+}
+
+
+
+
+
